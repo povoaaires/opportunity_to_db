47a96ffe484370799fdec8200342f410fb4c3fc2
Benedito Povoa <dito.povoa@gmail.com>
2024-02-12 21:11:42 -0300

    check exist client

--- opp_to_db/__init__.py
+++ opp_to_db/__init__.py
@@ -1,25 +1,57 @@
 import logging
 import pyodbc
+import json
 import azure.functions as func
+from datetime import datetime
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
-        req = req.json()
+        req = req.get_json()
         logging.info("Registro lido com Sucesso em seu devido formado")
 
-    except Exception:
-        logging.error("Erro ao ler o body, dicionário fora do padrão estabelecido")
+    except Exception as ex:
+        logging.error(f"Erro ao ler o body, dicionário fora do padrão estabelecido -> {ex}")
         return func.HttpResponse(
              "Erro ao ler o body, dicionário fora do padrão estabelecido",
              status_code=500
         )
     
-    connect = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=NTB-64VKH93;DATABASE=crm_auto_fast;UID=usersystem;PWD=8fdKJl3$nlNv3049jsKK")
-    cursor = connect.cursor()
-    cursor.execute("SELECT * FROM dbo.Clientes")
-    row = cursor.fetchone()
-    print(row)
 
+    try:
+        query = f"SELECT CPF from Clientes where CPF = '{req['Cpf']}';"
+        row = ConectSql('dml',query)
+        if row == None:
+            query = f"INSERT INTO Clientes ([Nome],[Sobrenome],[Email],[Telefone],[DataNascimento],[Endereco],\
+                [Cidade],[Estado],[CEP],[Pais],[DataCadastro],[CPF])\
+                    VALUES ({req['NomePessoa']},{req['NomePessoa'].split(" ")[1]},{req['Email']}, \
+                        {req['Telefone']},{req['DataNascimento']},{req['Endereco']},{req['Cidade']},\
+                            {req['Estado']},{req['CEP']},{str(datetime.now())},{req['NomePessoa']},{req['Cpf']})"
+            
+            row = ConectSql('ddl',query)
+            print(row)
+            
+    except:
+        pass
+
+
+
+
+
+def ConectSql(tipo,query):
+    try:
+        cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=NTB-64VKH93\SQLEXPRESS;DATABASE=crm_auto_fast;Trusted_Connection=yes;")
+        cursor = cnxn.cursor()  
+        if tipo == 'ddl':
+            count = cursor.execute(query).rowcount
+            cnxn.commit()
+            fim = str(count)
+            return fim
+        elif tipo == 'dml':
+            cursor.execute(query) 
+            row = cursor.fetchone() 
+            return str(row)
+    except Exception as ex:
+        return ex
